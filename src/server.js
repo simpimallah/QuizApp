b435dfd7ef1528b02086c2464dd1e3eab9087261
radhkrishna1102 <divinerktechnology@gmail.com>
2025-07-10 01:10:52 +0530

    port add

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 
-const PORT = process.env.MONGO_URI ;
+const PORT = process.env.PORT | 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
