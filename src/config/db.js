848bc8192910d7b0954fc2c1dd4c227e9627e595
radhkrishna1102 <divinerktechnology@gmail.com>
2025-07-09 23:21:15 +0530

    changes and update mongourl

--- src/config/db.js
+++ src/config/db.js
@@ -1,38 +1,13 @@
-// const mongoose = require("mongoose");
-// require("dotenv").config();
 
-// const connectDB = async () => {
-//   try {
-//     const conn = await mongoose.connect(process.env.MONGO_URI);
-//     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
-//   } catch (error) {
-//     console.error("❌ MongoDB Connection Failed:", error.message);
-//     process.exit(1);
-//   }
-// };
 
-// module.exports = connectDB;
-
-require('dotenv').config(); // Must be at the top
-
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  const mongoUri = process.env.MONGO_URI | 1000;
-
-  if (!mongoUri) {
-    console.error('❌ MONGO_URI is undefined');
-    process.exit(1); // Exit with failure
-  }
-
   try {
-    await mongoose.connect(mongoUri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('✅ MongoDB Connected');
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log("✅ MongoDB connected",conn);
   } catch (err) {
-    console.error('❌ MongoDB Connection Failed:', err.message);
+    console.error("❌ MongoDB Connection Failed:", err.message);
     process.exit(1);
   }
 };
