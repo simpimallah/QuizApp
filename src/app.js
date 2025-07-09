cb722241d378e64553a9b82abc937e531cf04a71
radhkrishna1102 <divinerktechnology@gmail.com>
2025-07-10 01:31:13 +0530

    add get

--- src/app.js
+++ src/app.js
@@ -23,6 +23,9 @@ app.use(express.json()); // To parse JSON bodies
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/quiz", require("./routes/quizRoutes"));
 app.use("/api/admin", require("./routes/adminRoutes"));
+app.get('/', (req, res) => {
+  res.send('🚀 Server is live on Render!');
+});
 
 // Error Handlers
 app.use(notFound);       // 404
