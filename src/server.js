5d7032d2a19457b1c9e4dafd020282e3abf0f47b
radhkrishna1102 <divinerktechnology@gmail.com>
2025-07-10 00:52:36 +0530

    remove port

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 
-const PORT = process.env.MONGO_URI || process.env.PORT;
+const PORT = process.env.MONGO_URI ;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
